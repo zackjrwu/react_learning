285b6662eec88bd1c83ab993e6d8228fec953b31
Zack <zack@tenmax.io>
2019-10-31 11:47:09 +0800

    index 註解, 修改 render element ID

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,10 +1,15 @@
+//  import React 組件
 import React from 'react';
+//  import ReactDOM 組件
 import ReactDOM from 'react-dom';
+//  import 當下目錄的 css file
 import './index.css';
+//  import 當下的 App component
 import App from './App';
+//  ??
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+//  使用 ReactDOM.render(欲繪製的組件, 想繪製在哪個 tag 裡面) 來繪製組件
+ReactDOM.render(<App />, document.getElementById('rootgg'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
